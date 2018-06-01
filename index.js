07c73d102207278191a5aadf9c1482f9bb823691
Karly Mareka <karly.mareka@gmail.com>
2018-06-01 19:56:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -60,5 +60,12 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+   if (cardNumber === undefined) {
+    return ("Sorry, we don't have a credit card on file for you.");
+  }
+  else {
+    var totalAmount = total();
+    cart = []; 
+    return (`Your total cost is $${totalAmount}, which will be charged to the card ${cardNumber}.`)
+  }
 }
